082b35b31344c0d30291977a402950f77d7bb593
MeTraFaN <metrafan1@mail.ru>
2017-05-04 16:38:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,4 @@
-var app = require('express')();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-
-server.listen(5000);
-app.use(express.static(__dirname + '/public'));
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
-});
-
-io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
-});
-
-
-
-
-
-
-/*var express = require('express');
+var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -39,6 +14,6 @@ app.get('/', function(request, response) {
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
-});*/
+});
 
 
